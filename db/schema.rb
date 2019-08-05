0d3aa0916767992a2cb0ab1efb1eb1c17b156450
Marcin Zadka <gamboox@gmail.com>
2019-08-05 18:30:38 +0200

    Add Google Analytics

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_103253) do
+ActiveRecord::Schema.define(version: 2019_08_05_162533) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
