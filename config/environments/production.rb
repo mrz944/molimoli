db0b95c6145dd7a785572f186daddd469996c7cc
Marcin Zadka <gamboox@gmail.com>
2019-08-08 19:01:05 +0200

    Set sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,11 +68,25 @@ Rails.application.configure do
     host: 'www.molimoli.pl'
   }
 
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.perform_deliveries = true  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    user_name: ENV['SENDGRID_USER'],
+    password: ENV['SENDGRID_PASSWORD'],
+    authentication: :plain,
+    domain: 'molimoli.pl',
+    enable_starttls_auto: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
